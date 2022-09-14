e94da39ea5493df01e425e85b2f26ca01eba6cbe
bijoyone <bijoyone@yahoo.com>
2022-09-14 12:11:28 +0600

    Update login.jsx

--- pages/admin/login.jsx
+++ pages/admin/login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
 
   const handleClick = async () => {
     try {
-      await axios.post("http://localhost:3000/api/login", {
+      await axios.post("https://vercel.com/bijoyone/pizza/DJyqgoLjHhJ98aEHU3FQbaEVQojx/api/login", {
         username,
         password,
       });
