3a9918d8c36b4d60792785b3c7f3e300336f5ddd
bijoyone <bijoyone@yahoo.com>
2022-09-14 12:47:49 +0600

    Update [id].jsx

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -97,7 +97,7 @@ const Product = ({ pizza }) => {
 
 export const getServerSideProps = async ({ params }) => {
   const res = await axios.get(
-    `https://vercel.com/bijoyone/pizza/DJyqgoLjHhJ98aEHU3FQbaEVQojx/api/products/${params.id}`
+    `https://pizza-shop-king.vercel.app/api/products/${params.id}`
   );
   return {
     props: {
