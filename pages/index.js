c9acf8a6ad869855511ae6765428aed7ef7e63a2
bijoyone <bijoyone@yahoo.com>
2022-09-14 12:14:39 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export const getServerSideProps = async (ctx) => {
     admin = true;
   }
 
-  const res = await axios.get("http://localhost:3000/api/products");
+  const res = await axios.get("https://vercel.com/bijoyone/pizza/DJyqgoLjHhJ98aEHU3FQbaEVQojx/api/products");
   return {
     props: {
       pizzaList: res.data,
